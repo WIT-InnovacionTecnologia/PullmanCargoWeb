ba86f381a2c3a84aa02f339dc283712dc98d76f2
hlcxpl <hlcxpl@gmail.com>
2026-02-13 13:59:02 -0300

    style: Increase logo size in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -62,7 +62,7 @@ export function Header() {
             <img
               src="/brand/LOGO2 CARGO.png"
               alt="Pullman Cargo"
-              className="h-8 md:h-10 w-auto object-contain"
+              className="h-10 md:h-12 w-auto object-contain"
             />
           </Link>
 
